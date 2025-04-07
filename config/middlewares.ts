aa92aa23d988ae7c2ee97823ceeff9fc324b8086
Tanguy <tanguy.cirillo@gmail.com>
2025-04-07 15:47:13 +0200

    feat: init project

---
+++ config/middlewares.ts
@@ -0,0 +1,40 @@
+module.exports = [
+  'strapi::errors',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '833910365045-portfolio.s3.eu-west-3.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '833910365045-portfolio.s3.eu-west-3.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
