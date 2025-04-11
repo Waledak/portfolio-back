3cdb17c8d6693c61329834d00bc9144a5303361e
Tanguy <tanguy.cirillo@gmail.com>
2025-04-11 23:09:56 +0200

    feat: add dockerization for strapi, git workflow and homepage data

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,54 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface AtomTag extends Struct.ComponentSchema {
+  collectionName: 'components_atom_tags';
+  info: {
+    description: '';
+    displayName: 'tag';
+    icon: 'cloud';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+  };
+}
+
+export interface CardSkill extends Struct.ComponentSchema {
+  collectionName: 'components_card_skills';
+  info: {
+    description: '';
+    displayName: 'skill';
+    icon: 'book';
+  };
+  attributes: {
+    logo: Schema.Attribute.String;
+    name: Schema.Attribute.String & Schema.Attribute.Required;
+    tags: Schema.Attribute.Component<'atom.tag', true>;
+  };
+}
+
+export interface MoleculeContactLink extends Struct.ComponentSchema {
+  collectionName: 'components_molecule_contact_links';
+  info: {
+    displayName: 'contactLink';
+    icon: 'globe';
+  };
+  attributes: {
+    logo: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.Unique;
+    name: Schema.Attribute.String & Schema.Attribute.Required;
+    url: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.Unique;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'atom.tag': AtomTag;
+      'card.skill': CardSkill;
+      'molecule.contact-link': MoleculeContactLink;
+    }
+  }
+}
