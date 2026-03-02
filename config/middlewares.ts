6813091dd4f6ef2200a654cfade595947ca6d3e0
Tanguy <tanguy.cirillo@gmail.com>
2026-03-02 18:05:08 +0100

    fix: cors s3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,30 +1,60 @@
-module.exports = ({ env }) => [
+module.exports = ({ env }) => {
+  const bucket = env("AWS_BUCKET");
+  const region = env("AWS_REGION");
+
+  return [
     "strapi::errors",
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'res.cloudinary.com', // cloudinary images
-            'lh3.googleusercontent.com', // google avatars
-            'platform-lookaside.fbsbx.com', // facebook avatars
-            'dl.airtable.com', // strapi marketplace
-            `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`
-          ],
-          'media-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
-          upgradeInsecureRequests: null,
+
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+
+            "img-src": [
+              "'self'",
+              "data:",
+              "blob:",
+
+              `https://s3.${region}.amazonaws.com`,
+              `https://${bucket}.s3.${region}.amazonaws.com`,
+              "https://*.amazonaws.com",
+            ],
+
+            "media-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              `https://s3.${region}.amazonaws.com`,
+              `https://${bucket}.s3.${region}.amazonaws.com`,
+              "https://*.amazonaws.com",
+            ],
+
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-    'strapi::logger',
-    "strapi::cors",
+
+    {
+      name: "strapi::cors",
+      config: {
+        enabled: true,
+        origin: [
+          "https://tanguycirillo.fr",
+          "https://www.tanguycirillo.fr",
+          "https://api.tanguycirillo.fr",
+
+          "http://localhost:3000",
+          "http://localhost:1337",
+        ],
+        headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      },
+    },
+
     "strapi::poweredBy",
     "strapi::logger",
     "strapi::query",
@@ -33,3 +63,4 @@ module.exports = ({ env }) => [
     "strapi::favicon",
     "strapi::public",
   ];
+};
